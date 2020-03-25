3a95682e8547fc911901174853348a211b1f3f49
Luis Eduardo Berrospi <berzavlu@gmail.com>
2020-03-25 01:52:00 -0500

    pruebas con immer js y useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import produce from 'immer'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ENVIA_TEXTO = 'enviar-texto'
+const RESET_TEXTO = 'resetear-texto'
+
+const initialState = {
+  mensaje: '',
 }
 
-export default App;
+const testReducer = (state, action) => {
+  switch (action.type) {
+    case ENVIA_TEXTO:
+      return produce(state, (draftState) => {
+        draftState.mensaje = 'holaaa'
+      })
+    case RESET_TEXTO: // sin immer
+      return {
+        ...state,
+        mensaje: '',
+      }
+    default:
+      return new Error('upsi')
+  }
+}
+
+const testReducerDos = (draft, action) => {
+  switch (action.type) {
+    case ENVIA_TEXTO:
+      draft.mensaje = 'holaaaa 2'
+      return
+    case RESET_TEXTO:
+      draft.mensaje = ''
+      return
+    default:
+      return new Error('upsi')
+  }
+}
+
+const testReducerImmer = produce(testReducerDos)
+
+function App() {
+  const [state, dispatch] = useReducer(testReducer, initialState)
+  const [stateDos, dispatchDos] = useReducer(testReducerImmer, initialState)
+
+  const dispara = () => {
+    const action = {
+      type: ENVIA_TEXTO,
+    }
+    dispatch(action)
+  }
+
+  const disparaDos = () => {
+    const action = {
+      type: ENVIA_TEXTO,
+    }
+    dispatchDos(action)
+  }
+
+  const regresa = () => {
+    const action = {
+      type: RESET_TEXTO,
+    }
+    dispatch(action)
+    dispatchDos(action)
+  }
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {state.mensaje}
+        {stateDos.mensaje}
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div onClick={dispara}>
+            mostrar mensaje
+            <br /> 1
+          </div>
+          <div onClick={disparaDos}>
+            mostrar mensaje
+            <br /> 2
+          </div>
+        </div>
+
+        <p onClick={regresa}>regresa al estado inicial</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
